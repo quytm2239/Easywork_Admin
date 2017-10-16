bfa65eb8ec53d5184f89b7b7dd778ab618974446
quytm2239 <quytm2239@gmail.com>
2017-10-16 22:22:33 +0700

    Update logic scroll

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -4,18 +4,61 @@ import '../../components/footer/footer.js';
 
 Template.App_body.onCreated(function helloOnCreated() {
     // counter starts at 0
-    this.preScrollPosition = new ReactiveVar(0);
-    this.toggle = 1;
+    this.preScrollPosition = 0;
+    this.isScrollDown = false;
+    this.isBigFooter = true;
 });
 
+function updateFooterAndLayoutContainer(isScrollDown) {
+    if (isScrollDown) {
+        $('footer').height(30);
+        $('footer .footer-container').css('padding-top',0);
+        $('footer .footer-container').css('padding-bottom',0);
+
+        // Update Main layout container
+        $('.layout-container').css('height','-moz-calc(100% - (30px + 74px))');
+        $('.layout-container').css('height','-o-calc(100% - (30px + 74px))');
+        $('.layout-container').css('height','-webkit-calc(100% - (30px + 74px))');
+        $('.layout-container').css('height','calc(100% - (30px + 74px))');
+    } else {
+        $('footer').height(80);
+        $('footer .footer-container').css('padding-top',10);
+        $('footer .footer-container').css('padding-bottom',10);
+
+        // Update Main layout container
+        $('.layout-container').css('height','-moz-calc(100% - (80px + 74px))');
+        $('.layout-container').css('height','-o-calc(100% - (80px + 74px))');
+        $('.layout-container').css('height','-webkit-calc(100% - (80px + 74px))');
+        $('.layout-container').css('height','calc(100% - (80px + 74px))');
+    }
+}
 Template.App_body.events({
-    'click footer'(event, instance) {
-        if (instance.toggle == 1) {
-            $('footer').height(40);
-            instance.toggle = 0;
+    'scroll .layout-container'(event, instance){
+        var currentPosition = event.target.scrollTop;
+        if (currentPosition > instance.preScrollPosition) {
+            // ScrollDown
+            if (instance.isScrollDown != true) {
+                updateFooterAndLayoutContainer(true);
+                instance.isScrollDown = true;
+                instance.isBigFooter = false;
+            }
         } else {
-            $('footer').height(80);
-            instance.toggle = 1;
+            if (instance.isScrollDown != false) {
+                updateFooterAndLayoutContainer(false);
+                instance.isScrollDown = false;
+                instance.isBigFooter = true;
+            }
+        }
+        instance.preScrollPosition = currentPosition;
+    },
+
+    'click footer'(event, instance) {
+        if (instance.isBigFooter == false) {
+            instance.isBigFooter = true;
+            updateFooterAndLayoutContainer(false);
+        } else {
+            instance.isBigFooter = false;
+            updateFooterAndLayoutContainer(true);
         }
     }
 });
