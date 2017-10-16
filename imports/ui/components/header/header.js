90f8c5965327f765bc481b7405c329a2086564a7
long.dang.thanh <long.dang.thanh@sofiamedix.vn>
2017-10-16 16:25:58 +0700

    update header

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -6,11 +6,37 @@ Template.header.helpers({
 });
 
 Template.header.events({
+    'mouseenter #products'(event, instance) {
+        $('.products').slideDown( "slow");
+    },
+    'mouseenter #downloads'(event, instance) {
+        $('.downloads').slideDown( "slow");
+    },
+    'mouseenter #applications'(event, instance) {
+        $('.project').slideDown( "slow");
+    },
     'mouseenter #project'(event, instance) {
-        $('.project').show();
+        $('.project').slideDown( "slow");
+    },
+    'mouseenter #freeware'(event, instance) {
+        $('.freeware').slideDown( "slow");
+    },
+
+    //leave mouse
+    'mouseleave #products'(event, instance) {
+        $('.cbp-hrsub').hide();
+    },
+    'mouseleave #downloads'(event, instance) {
+        $('.cbp-hrsub').hide();
     },
     'mouseleave #project'(event, instance) {
-        $('.project').hide();
+        $('.cbp-hrsub').hide();
+    },
+    'mouseleave #applications'(event, instance) {
+        $('.cbp-hrsub').hide();
+    },
+    'mouseleave #freeware'(event, instance) {
+        $('.cbp-hrsub').hide();
     }
 
 });
