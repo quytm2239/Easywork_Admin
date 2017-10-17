df7a72dec6b2ac5e177760c1dc55876e21073a16
quytm2239 <quytm2239@gmail.com>
2017-10-17 17:11:42 +0700

    Update footer

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -12,8 +12,8 @@ Template.App_body.onCreated(function helloOnCreated() {
 function updateFooterAndLayoutContainer(isScrollDown) {
     if (isScrollDown) {
         $('footer').height(30);
-        $('footer .footer-container').css('padding-top',0);
-        $('footer .footer-container').css('padding-bottom',0);
+        // $('footer .footer-container').css('padding-top',0);
+        // $('footer .footer-container').css('padding-bottom',0);
 
         // Update Main layout container
         $('.layout-container').css('height','-moz-calc(100% - (30px + 60px))');
@@ -21,17 +21,32 @@ function updateFooterAndLayoutContainer(isScrollDown) {
         $('.layout-container').css('height','-webkit-calc(100% - (30px + 60px))');
         $('.layout-container').css('height','calc(100% - (30px + 60px))');
     } else {
-        $('footer').height(80);
-        $('footer .footer-container').css('padding-top',10);
-        $('footer .footer-container').css('padding-bottom',10);
+        $('footer').height(60);
+        // $('footer .footer-container').css('padding-top',10);
+        // $('footer .footer-container').css('padding-bottom',10);
 
         // Update Main layout container
-        $('.layout-container').css('height','-moz-calc(100% - (80px + 60px))');
-        $('.layout-container').css('height','-o-calc(100% - (80px + 60px))');
-        $('.layout-container').css('height','-webkit-calc(100% - (80px + 60px))');
-        $('.layout-container').css('height','calc(100% - (80px + 60px))');
+        $('.layout-container').css('height','-moz-calc(100% - (60px + 60px))');
+        $('.layout-container').css('height','-o-calc(100% - (60px + 60px))');
+        $('.layout-container').css('height','-webkit-calc(100% - (60px + 60px))');
+        $('.layout-container').css('height','calc(100% - (60px + 60px))');
     }
 }
+
+$(function () {
+    $( window ).resize(function() {
+            changeLayout();
+    });
+});
+
+function changeLayout () {
+    if (parseInt($(window).width()) <= 800) {
+
+    } else {
+
+    }
+}
+
 Template.App_body.events({
     'scroll .layout-container'(event, instance){
         var currentPosition = event.target.scrollTop;
